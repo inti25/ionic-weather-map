2cb68e80c6f061807625103f38fb508d3100b7fe
inti25 <nqhung92@gmail.com>
2019-07-12 17:29:48 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+This repository is example application using Ionic 4 + Angular 7
+
+##This application contains:
+
+ * init and show google maps, maker and window info using [Angular Google Maps](https://angular-maps.com/)
+ * call backend api to get weather data using [Openweathermap API](https://openweathermap.org/api)
+
+## Installation
+
+Install dependencies
+```sh
+npm install
+```
+## Run the ionic app
+
+You try this app on the browser by running the following command:
+```sh
+ionic serve
+```
---
+++ src/app/service/weather-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherServiceService {
+
+  baseUrl = 'https://api.openweathermap.org/data/2.5/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCurrentWeatherByLatLon(lat: number, lon: number) {
+    return this.httpClient.get(this.baseUrl + 'weather?appid=413b4f3fe224ebb24c6466edb0bdc76b&units=metric&lang=vi&lat=' + lat + '&lon=' + lon);
+  }
+}
